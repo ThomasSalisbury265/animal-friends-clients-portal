a5680eb8d751483790d6e53715f9a1a2c35859c4
Tom Salisbury <Tom.Salisbury@oneiota.co.uk>
2024-11-28 20:50:31 +0000

    update dashboard with load more logic

--- src/app/features/clients/dashboard/dashboard.component.ts
+++ src/app/features/clients/dashboard/dashboard.component.ts
@@ -1,19 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { ClientsService } from '../clients.service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ClientDashboard } from '../../../../shared/models/ui/client-dashboard.interface';
+import { Observable } from 'rxjs';
+import { ClientsService } from '../clients.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
   private clientsService: ClientsService = inject(ClientsService);
-  public clients: ClientDashboard[] = [];
+  private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private loadMore: number = 25;
+  private loadMoreBy: number = 5;
+  public clients$: Observable<Array<ClientDashboard>> | undefined;
+
   ngOnInit(): void {
-    this.clientsService.getClients();
-    this.clientsService.dashboardClients$.subscribe((clients) => this.clients.push(...clients))
+    this.clients$ = this.clientsService.dashboardClients$
+  }
+
+  public onClickClient(id: string) {
+    this.router.navigate([id], { relativeTo: this.route})
+  }
+
+  public loadMoreClients() {
+    this.clientsService.getClients(this.loadMore);
+    this.loadMore += this.loadMoreBy;
   }
 }
