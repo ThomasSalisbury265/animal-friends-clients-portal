5436e9e3c020badb047f1419214598da6d49fe94
Tom Salisbury <Tom.Salisbury@oneiota.co.uk>
2024-11-28 19:46:28 +0000

    add generic get method to api service

--- src/core/services/api.service.ts
+++ src/core/services/api.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from '../../environment/environment';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,15 @@ export class ApiService {
   private http = inject(HttpClient);
 
   constructor() { }
+
+  public get<T>(): Observable<T> {
+    return this.http.get<T>(`${this._baseUrl}?results=20&page=1`).pipe(
+      catchError(this.handleHttpError)
+    )
+  }
+
+  private handleHttpError(error: HttpErrorResponse) {
+    console.log('API Error: ', error);
+    return throwError(() => error.message);
+  }
 }
